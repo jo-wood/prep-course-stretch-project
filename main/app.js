9d5af82906a6298e7a356eaed39bc22844fde1e2
Joanna Wood <caitlinjoannawood@gmail.com>
2019-05-07 15:34:42 +0000

    rendered data items to UI

--- main/app.js
+++ main/app.js
@@ -1,4 +1,29 @@
 /*jshint esversion:6*/
 
 
-$("a").addClass("test");
+function drawBarChart(data, options, element){
+
+for (let num of data) {
+  var newdiv = document.createElement( "div" ),
+    existingdiv1 = document.getElementById( "root" );
+
+    $(newdiv).append(num);
+
+console.log($( "body" ).append([ newdiv, existingdiv1 ]));
+
+
+}
+
+
+
+
+
+
+
+} //drawBarChart
+
+drawBarChart([1, 2, 3, 4, 5]);
+
+// ie data = [1, 2, 3, 4, 5]
+// ie options should be object for options of chart (width, height of chart)
+// ie element should be DOM element or Jquery element that the chart will get rendered into
