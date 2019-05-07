cff90042a9abc66d73db7685c7dcaf0a6f0f1ce8
Joanna Wood <caitlinjoannawood@gmail.com>
2019-05-07 19:31:32 +0000

    bar height correctly changing based on data value

--- main/app.js
+++ main/app.js
@@ -8,12 +8,10 @@ for (let num of data) {
     $(newdiv).append(num);
     $(newdiv).addClass("bar-num");
 
-if (num === 2) {
-  let newwidth = 200 + "px";
-  console.log(newwidth);
-  $(newdiv).css({"margin-right" : "$(newwidth)"});
+  let newheight = num*10 + "%";
+  $(newdiv).css("height" , newheight);
 
-}
+
 
 console.log($( "#chart" ).append(newdiv));
 
