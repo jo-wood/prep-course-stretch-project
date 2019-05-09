cc7f450c3933797c5739fd128b33331ddda5bb15
Joanna Wood <caitlinjoannawood@gmail.com>
2019-05-09 15:00:19 +0000

    added id's to each bar div

--- main/app.js
+++ main/app.js
@@ -5,15 +5,24 @@ function drawBarChart(data, options, element){
 
 
 let revData = data.reverse(); // bc flipped in css for styling
+let barMargins = 100 - data.length;
 
-/** separate each data entry into own bar:**/
+
+/** create chart **/
+
+
+/** add bars **/
 for (let num of revData) {
   var newdiv = document.createElement( "div" );
     $(newdiv).append(num);
     $(newdiv).addClass("bar-num");
-    //$(newdiv).attr(".bar-num", num);
+    $('.bar-num:last').attr('id', num);
 
-/** create each bars size based on entry value **/
+/** set bar widths **/
+  let barWidth = Math.floor(barMargins/data.length) + "%";
+    $(newdiv).css("width", barWidth); // bar width same for all
+
+/** create each bars height based on entry value **/
   let newheight = num*10 + "%";
   $(newdiv).css("height" , newheight);
 
@@ -32,7 +41,7 @@ console.log($( "#chart" ).append(newdiv));
 
 } //drawBarChart
 
-drawBarChart([1, 4, 2, 4, 6]);
+drawBarChart([1, 4, 2, 10, 6, 5]);
 
 // ie data = [1, 2, 3, 4, 5]
 // ie options should be object for options of chart (width, height of chart)
